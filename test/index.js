4977f8c7c12a0f266e1916a207a21fe64af056e1
Łaniewski Bartosz <laniewski.bartozzz@gmail.com>
2017-10-07 15:08:03 +0200

    ESLinter fix

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 "use strict";
 
-let override = require( "../dist" );
-let assert = require( "assert" );
+let override = require("../dist");
+let assert = require("assert");
 
 let obj = {
     a: "I'm a default value!",
@@ -30,38 +30,38 @@ let obj = {
  * @param  {Object} b
  * @return {boolean}
  */
-function equal( a, b ) {
-    if ( a.length !== b.length ) {
+function equal(a, b) {
+    if (a.length !== b.length) {
         return false;
     }
 
-    for ( let i = a.length; i--; ) {
-        if ( a[i] !== b[i] ) return false;
+    for (let i = a.length; i--;) {
+        if (a[i] !== b[i]) return false;
     }
 
     return true;
 }
 
-describe( "json-overrides", () => {
-    it( "should override json", () => {
-        assert.equal( true, equal( {
+describe("json-overrides", () => {
+    it("should override json", () => {
+        assert.equal(true, equal({
             a: "I'm a default value for project A!",
             b: "I'll never change!",
-        }, override( obj, "projectA" ) ) );
+        }, override(obj, "projectA")));
 
-        assert.equal( true, equal( {
+        assert.equal(true, equal({
             a: "I'm a default value for project B!",
             b: "I'll never change!",
-        }, override( obj, "projectB" ) ) );
+        }, override(obj, "projectB")));
 
-        assert.equal( true, equal( {
+        assert.equal(true, equal({
             a: "I'm a default value for project C!",
             b: "... or will I?",
-        }, override( obj, "projectC" ) ) );
-    } );
+        }, override(obj, "projectC")));
+    });
 
-    it( "should return undefined if json cannot be overrided", () => {
-        assert.equal( undefined, override( obj, "projectD" ) );
-        assert.equal( undefined, override( {a: true}, "projectD" ) );
-    } );
-} );
+    it("should return undefined if json cannot be overrided", () => {
+        assert.equal(undefined, override(obj, "projectD"));
+        assert.equal(undefined, override({a: true}, "projectD"));
+    });
+});
